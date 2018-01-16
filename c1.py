fe8bcd020be5d1a67d68bf04fafbe39eba500488
chandan1105 <35484326+chandan1105@users.noreply.github.com>
2018-01-16 15:04:25 +0530

    Add files via upload

---
+++ c1.py
@@ -0,0 +1,5 @@
+# adding 2 number
+a=10
+b=20
+c=a+b
+print("addition of ",a,and ,b," is"c)
---
+++ c2.py
@@ -0,0 +1,14 @@
+#addition
+a=10
+b=20
+c=a+b
+print("addition of two no. a and b",c)
+#substraction 
+c=a-b
+print("substraction",c)
+#division
+c=a/b
+print("division",c)
+#multiplication
+c=a*b
+print("multiplication",c)
