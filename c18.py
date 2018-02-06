012df29db6a2c619f718d96bdbf671e18e37cc1e
chandan1105 <35484326+chandan1105@users.noreply.github.com>
2018-02-06 15:21:45 +0530

    Add files via upload

---
+++ c18.py
@@ -0,0 +1,4 @@
+#program using for loop
+a=[1,2,3,4,5,6,7]
+for i in a:
+    print(i)
---
+++ cx.py
@@ -0,0 +1,8 @@
+#program of odd and odd no. using for loop
+a=[1,2,3,4,5,6,7]
+for i in a:
+    if i%2==0:    
+      print("even no.",i)
+    if i%2==1:
+      print("odd no.",i)
+      
